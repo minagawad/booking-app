5ca1a5f8af7c190ad7cfdff202ff9ccc8feffc3b
mina gerges <Mina.A@eltizam.sa>
2025-10-21 13:07:52 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var confernceName = "Go Confernce"
+
+	fmt.Println("welcome to our ", confernceName, "booking application")
+	fmt.Println("Get your ticket here to attend")
+
+}
