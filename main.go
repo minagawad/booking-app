c937c96cdfa12f0e41e61eae0623a51f1a2c2a12
mina gerges <Mina.A@eltizam.sa>
2025-10-22 18:07:31 +0300

    working wuth loops and arrays

--- main.go
+++ main.go
@@ -1,34 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var confernceName = "Go Confernce"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
+	bookings := []string{}
+
 	fmt.Printf("welcome to our %v booking application", confernceName)
 	fmt.Println("We have total ", conferenceTickets, "tickets and ", remainingTickets, "are still available")
-
 	fmt.Println("Get your ticket here to attend")
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTicket uint
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTicket int
+		fmt.Println("enter your first name ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("enter your first name ")
-	fmt.Scan(&firstName)
+		fmt.Println("enter your last name ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("enter your last name ")
-	fmt.Scan(&lastName)
+		fmt.Println("enter your email  ")
+		fmt.Scan(&email)
 
-	fmt.Println("enter your email  ")
-	fmt.Scan(&email)
+		fmt.Println("enter number of tickets")
 
-	fmt.Println("enter number of tickets")
+		fmt.Scan(&userTicket)
 
-	fmt.Scan(&userTicket)
+		remainingTickets = remainingTickets - userTicket
 
-	fmt.Printf("Thank you %v %v for booking %v ticket , you will receive a confirmation email to you email address %v ", firstName, lastName, userTicket, email)
+		bookings = append(bookings, firstName+" "+lastName)
+		fmt.Printf("Thank you %v %v for booking %v ticket , you will receive a confirmation email to you email address %v ", firstName, lastName, userTicket, email)
+		fmt.Printf("there are %v tickets for %v", remainingTickets, confernceName)
+		fmt.Printf("there are all booking %v", bookings)
 
+		var firstNames []string
+
+		for _, booking := range bookings {
+
+			var names = strings.Fields(booking)
+			firstNames = append(firstNames, names[0])
+		}
+
+		fmt.Printf("there are all booking %v", firstNames)
+
+	}
 }
